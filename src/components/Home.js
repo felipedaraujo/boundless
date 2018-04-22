673a79a5cb9438996be42b0ff7b41782c849a0ed
Felipe de Araujo <me@felipedearaujo.com>
2018-04-21 22:50:35 -0400

    Replace ant design per bootstrap

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-import { Layout } from 'antd';
-const { Header, Content } = Layout;
 
 class Home extends Component {
   render() {
     return (
-      <Layout>
-        <Header>Header</Header>
-        <Content style={{ height: '100%' }}>Content</Content>
-      </Layout>
+      <div className="container-flex h-100 bg-warning d-flex align-items-center">
+        <div className="row d-flex justify-content-center">
+          <div className="col-10 d-flex flex-column justify-content-center align-items-center">
+            <h1 className="text-center display-4">
+              The way you feel is very important, so how are you today?
+            </h1>
+            <button className="btn btn-light btn-lg">Start now</button>
+          </div>
+        </div>
+      </div>
     );
   }
 }
